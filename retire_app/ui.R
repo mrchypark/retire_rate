e060f5bdd9347d1faff65f0c0a8a8211dbfcfa71
mrchypark <mrchypark@gmail.com>
2017-03-23 17:44:40 +0000

    run set

--- retire_app/ui.R
+++ retire_app/ui.R
@@ -3,12 +3,24 @@ library(ggiraph)
 
 fluidPage(
 
+  tags$head(
+    tags$style(HTML("
+      	
+        @import url(http://api.mobilis.co.kr/webfonts/css/?fontface=NanumGothicWeb); 
+      
+      * { font-family: 'NanumGothic', '나눔고딕','NanumGothicWeb', '맑은 고딕', 'Malgun Gothic', Dotum; }
+      }
+
+    "))
+  ),
+  
+  
   titlePanel("퇴직 소득세 계산"),
   
   sidebarLayout(
       sidebarPanel(
         sliderInput("amount", 
-                    label= "퇴직금 :", 
+                    label= "퇴직금 조절", 
                   value = 1,
                   step = 1,
                   min = 1, 
