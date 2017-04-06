fa3dfa7da360fbc82ec2de956b18321ad46d23c4
tggatsby <jun.creman@gmail.com>
2017-04-06 22:09:00 +0900

    title change2

--- retire_app/ui.R
+++ retire_app/ui.R
@@ -3,7 +3,7 @@ library(ggiraph)
 
 fluidPage( tags$head(tags$meta(name="viewport", content="width=device-width, initial-scale=1,minimum-scale=1.0,maximum-scale=1.0,user-scalable=no")),
 
-  titlePanel(h2("은퇴 소득세 계산 ㅋㅋㅋㅋㅋㅋ")
+  titlePanel(h2("은퇴 소득세 계산 ㅋㅋㅋㅋㅋㅋ 또 바꿈 ㅋㅋ ")
              ),
   
   sidebarLayout(
