9852e3c9968ca26563de685e15d54b5415b574f6
mrchypark <mrchypark@gmail.com>
2017-04-17 02:45:38 +0000

    run set

--- retire_app/server.R
+++ retire_app/server.R
@@ -4,7 +4,7 @@ function(input, output) {
 
   output$plot <- renderggiraph({
     g <- ggplot(tdat[tdat$verb == input$amount,], aes( x = as.factor(year), y = value)) + theme_minimal()
-    gg <- g + geom_bar_interactive(aes(fill=1,tooltip = paste0("<br>소득세: ", disp,"원<br> 세율: ",as.character(rate),"%")), stat="identity" ,
+    gg <- g + geom_bar_interactive(aes(fill=1,tooltip = paste0("소득세: ", disp,"원<br> 세율: ",as.character(rate),"%")), stat="identity" ,
                                    show.legend = FALSE)+ scale_x_discrete(name="근속년수",breaks=unique(tdat[,2]),
                                           labels=c("1 년후", "2 년후", "3 년후","5 년후","7 년후","10 년후","15 년후","20 년후"))+
       theme(axis.ticks = element_blank(), axis.text.y = element_blank(),axis.title.y = element_blank())+
